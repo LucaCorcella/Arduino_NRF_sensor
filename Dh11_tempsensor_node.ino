530baa43114a3c55dd458d95ab58a8eb45df81da
MarcoManca <marco.manca@isti.cnr.it>
2018-03-20 16:30:26 +0100

    Update Dh11_tempsensor_node.ino

--- Dh11_tempsensor_node.ino
+++ Dh11_tempsensor_node.ino
@@ -1,58 +1,60 @@
 #include "DHT.h"
-
 #include <nRF24L01.h>
 #include <SPI.h>
 #include <RF24.h>
+#include "printf.h"
 
 #define DHTPIN A0     // what analog pin we're connected to
-
 #define DHTTYPE DHT11   // DHT 22  (AM2302), AM2321
 
 RF24 radio(9, 10);
 
 const byte address[6] = "10000";
-
 bool ok;
 
 DHT dht(DHTPIN, DHTTYPE);
+float tmpTemperature = 0;
+float tmpHumidity = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.println("DHT11 test!");
 
-  Serial.println("DHTxx test!");
   dht.begin();
+  printf_begin();
 
-  radio.begin();  
+  radio.begin();
   radio.setPALevel(RF24_PA_LOW);
   radio.openWritingPipe(address);
-  radio.printDetails();
   radio.stopListening();
+  radio.printDetails();
+
 }
 
 void loop() {
-  // Wait a few seconds between measurements.
-
-
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
-
   float temperature[] = {t, h}; //send temperatuere and humidity as array
-
 
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
-     Serial.println(temperature[0]);
-     Serial.println(temperature[1]);
-     ok = radio.write(&temperature, sizeof(temperature));
-     if(!ok) {
+  if (tmpTemperature != t || tmpHumidity != h ) {
+    Serial.println(temperature[0]);
+    Serial.println(temperature[1]);
+    ok = radio.write(&temperature, sizeof(temperature));
+    if (!ok) {
       Serial.println("Failed to send!");
-     }
-    delay(5000);
+    } else {
+      tmpTemperature = t;
+      tmpHumidity = h;
+    }
+  }
+  delay(5000);
 
 }
